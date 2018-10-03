b90e0a5f617e693818188e151c25fa51912ab563
Jacob Luetzow <>
2018-10-02 18:16:31 -0700

    removed http import on index

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
 import bodyParser = require('body-parser')
 import express = require('express')
-import http = require('http')
 import { Router } from 'express'
 import { routes } from './api/routes'
 
-
 const app = express()
-const server = http.createServer(app)
-
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes(Router()))
 
-server.listen(3000, () => {
+app.listen(3000, () => {
 	console.log(('  App is running at http://localhost:%d in %s mode'), 3000, app.get('env'))
 	console.log('   Press CTRL-C to stop\n')
 })
